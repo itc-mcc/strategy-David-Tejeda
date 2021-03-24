6e4d7974da5ae2366675a05b5c17cd634fc75d55
David Tejeda <david_tejeda@itculiacan.edu.mx>
2021-03-24 02:37:27 +0000

    Primera prueba

--- src/main/java/miTiendita/descuento/AdultoMayor.java
+++ src/main/java/miTiendita/descuento/AdultoMayor.java
@@ -5,8 +5,11 @@ import miTiendita.venta.Venta;
 public class AdultoMayor implements IDesctoStrategy {
     @Override
     public float getDescto(Venta v) {
-        // TODO Auto-generated method stub
-        System.out.println("Sin descuento...");
-        return 0;
+        double total = 0;
+
+        for (LineaDeDetalle a : v.getLd()) {
+            total+=(a.getCtd()*a.getP().getPutin())*.05;            
+        }
+        return total;
     }    
 }
