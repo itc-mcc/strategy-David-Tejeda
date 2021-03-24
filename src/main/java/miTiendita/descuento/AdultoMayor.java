554c0cec28b90fb6c751e7db29b331ff21f6286e
David Tejeda <david_tejeda@itculiacan.edu.mx>
2021-03-24 02:47:50 +0000

    Segundo intento

--- src/main/java/miTiendita/descuento/AdultoMayor.java
+++ src/main/java/miTiendita/descuento/AdultoMayor.java
@@ -1,14 +1,15 @@
 package miTiendita.descuento;
 
+import miTiendita.venta.LineaDeDetalle;
 import miTiendita.venta.Venta;
 
 public class AdultoMayor implements IDesctoStrategy {
     @Override
-    public float getDescto(Venta v) {
+    public double getDescto(Venta v) {
         double total = 0;
 
         for (LineaDeDetalle a : v.getLd()) {
-            total+=(a.getCtd()*a.getP().getPutin())*.05;            
+            total+=(a.getCtd()*a.getP().getPunit())*.05;            
         }
         return total;
     }    
